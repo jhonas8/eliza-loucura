863ac35de259c6782b234b8766677a4433997384
jhonas8 <love.android62@gmail.com>
2024-12-09 14:00:02 -0300

    fix formatting

--- application_types/enums/firebase.py
+++ application_types/enums/firebase.py
@@ -3,11 +3,11 @@ from utils.get_env_var import get_environment
 
 
 def get_environment_suffix():
-    return "staging" if get_environment() == "STAGING" else ""
+    return "-staging" if get_environment() == "STAGING" else ""
 
 
 class FirebaseCollectionEnum(str, Enum):
     WEBHOOK_ENDPOINTS = f"xpaal-webhook-endpoints-{get_environment_suffix()}"
-    USERS = f"xpaal-notifications-users-{get_environment_suffix()}"
-    EXCHANGES = f"xpaal-possible-exchanges-{get_environment_suffix()}"
-    NOTIFICATIONS = f"xpaal-notifications-history-{get_environment_suffix()}"
+    USERS = f"xpaal-notifications-users{get_environment_suffix()}"
+    EXCHANGES = f"xpaal-possible-exchanges{get_environment_suffix()}"
+    NOTIFICATIONS = f"xpaal-notifications-history{get_environment_suffix()}"
